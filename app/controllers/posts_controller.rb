56e7139a5e0cd33d046214722f5a631259534ff8
adolfoamc95 <amontillacarmona95@gmail.com>
2018-05-03 21:57:59 +0200

    Añadidas funcionalidades create y index de post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,20 @@
 class PostsController < ApplicationController
+  def create_params
+    params.require(:post).permit(:description, :user_id)
+  end
+
+  def index
+    @posts = Post.all.order('created_at DESC')
+    render json: @posts
+  end
+
+  def create
+    @user = User.find(create_params[:user_id])
+    @post = @user.posts.new(create_params)
+    if @post.save
+      render json: @post
+    else
+      render json: @post.errors
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :description, presence: true, length: { maximum: 500 }
 end
